3ae5463d2e3fdffe4a26709f1fde0d9a9e2b4634
JonDev2023 <roupasee@gmail.com>
2023-02-14 00:30:18 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import re
+import os
+import long_responses as long
+
+def message_probabillity(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certain = 0
+    has_required_words = True
+
+    for word in user_message:
+        if word in recognised_words:
+            message_certain += 1
+
+    percentage = float(message_certain) / float(len(recognised_words))
+
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    if has_required_words or single_response:
+        return int(percentage*100)
+    else:
+        return 0
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probabillity(message, list_of_words)
+
+    # ###############
+    # RESPONSE
+    # ###############
+    response('Hello!', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_response=True)
+    response('I\'m doing fine, and you', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('Thank You', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+
+    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
+    response('Opening Cloud Games', ['open', 'cloud', 'games'], required_words=['cloud', 'games'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    # print(highest_prob_list)
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = check_all_messages(split_message)
+    return response
+
+while True:
+    response = get_response(input('You: '))
+    event = os.system('C:/PyBot_Data/')
+
+    print('Bot: ' + response)
